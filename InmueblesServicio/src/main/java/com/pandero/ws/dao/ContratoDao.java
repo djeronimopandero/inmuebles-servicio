2ec8f4f8acd8012f1542f19de66eaa0e42920707
afernandezl <afernandezl@PAN_SIS_DES2.pandero.com.pe>
2016-11-26 14:41:27 -0500

    sincronizacion de saf a caspio 26/11/2016

--- InmueblesServicio/src/main/java/com/pandero/ws/dao/ContratoDao.java
+++ InmueblesServicio/src/main/java/com/pandero/ws/dao/ContratoDao.java
@@ -1,8 +1,11 @@
 package com.pandero.ws.dao;
 
+import java.util.List;
+
 import com.pandero.ws.bean.Contrato;
 
 public interface ContratoDao {
 
 	public Contrato obtenerContratoSAF(String nroContrato) throws Exception;
+	public List<Contrato> getListContratoAlDia()throws Exception;
 }
