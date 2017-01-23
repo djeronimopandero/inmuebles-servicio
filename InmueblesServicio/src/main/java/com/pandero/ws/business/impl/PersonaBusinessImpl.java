6bdcbb92676b23896abe4cd14e5d647bbed29619
Alan <acabreraa@pandero.com.pe>
2017-01-23 15:00:55 -0500

    descarga seguro

--- InmueblesServicio/src/main/java/com/pandero/ws/business/impl/PersonaBusinessImpl.java
+++ InmueblesServicio/src/main/java/com/pandero/ws/business/impl/PersonaBusinessImpl.java
@@ -1,11 +1,12 @@
 package com.pandero.ws.business.impl;
 
-import java.io.OutputStream;
-import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Locale;
 import java.util.Map;
 
 import org.slf4j.Logger;
@@ -13,7 +14,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import com.pandero.ws.bean.Inversion;
 import com.pandero.ws.bean.Pedido;
 import com.pandero.ws.bean.PersonaSAF;
 import com.pandero.ws.business.PersonaBusiness;
@@ -21,8 +21,6 @@ import com.pandero.ws.dao.LiquidacionDao;
 import com.pandero.ws.dao.PersonaDao;
 import com.pandero.ws.service.PedidoService;
 import com.pandero.ws.service.PersonaService;
-import com.pandero.ws.util.Constantes;
-import com.pandero.ws.util.JsonUtil;
 import com.pandero.ws.util.ServiceRestTemplate;
 
 @Component
@@ -90,12 +88,78 @@ public class PersonaBusinessImpl implements PersonaBusiness{
 			current.remove("Seguro_modalidad");
 			if("".equals(String.valueOf(current.get("NOMBRE")))){
 				current.put("NOMBRE", current.get("Persona_RazonSocial"));
+				current.put("CODPAISPREST", "");
+				current.put("PAISPREST", "");
 			}
+			current.put("TIPOOPER", "GARANTIA");
+			current.put("DESCRIPTIPOPLAN", "");
+			current.put("CODPAISRIESGO", "189");
+			current.put("PAISRIESGO", "PERÚ");
+			
+			if(current.get("PRIMAANUAL")!= null && !"".equals(String.valueOf(current.get("PRIMAANUAL")))){
+				current.put("PRIMAANUAL", String.format(Locale.ROOT, "%.2f", Double.parseDouble(String.valueOf(current.get("PRIMAANUAL"))) * 12) );
+			}else{
+				current.put("PRIMAANUAL", "" );
+				current.put("TASANETAANUAL", "");
+			}
+			
 			current.remove("Persona_RazonSocial");
 			current.remove("idPersona");
 		}
+		List<Map<String,Object>> resultFinal = new ArrayList<Map<String,Object>>();
+		if(result!=null && !result.isEmpty()){
+			for(Map<String,Object> current:result){
+				Map<String,Object> tmpMap = new LinkedHashMap<String,Object>();
+				tmpMap.put("NOMBRE", String.valueOf(current.get("NOMBRE")));
+				tmpMap.put("APELL1", String.valueOf(current.get("APELL1")));
+				tmpMap.put("APELL2", String.valueOf(current.get("APELL2")));
+				tmpMap.put("TIPODOC", String.valueOf(current.get("TIPODOC")));
+				tmpMap.put("NUMDOC", String.valueOf(current.get("NUMDOC")));
+				tmpMap.put("FECNACIMIENTO", String.valueOf(current.get("FECNACIMIENTO")));
+				tmpMap.put("SEXO", String.valueOf(current.get("SEXO")));
+				tmpMap.put("ESTADOCIVIL", String.valueOf(current.get("ESTADOCIVIL")));
+				tmpMap.put("CODPAISPREST", String.valueOf(current.get("CODPAISPREST")));
+				tmpMap.put("PAISPREST", String.valueOf(current.get("PAISPREST")));
+				tmpMap.put("CODDPTOPREST", String.valueOf(current.get("CODDPTOPREST")));
+				tmpMap.put("DPTOPREST", String.valueOf(current.get("DPTOPREST")));
+				tmpMap.put("CODPROVPREST", String.valueOf(current.get("CODPROVPREST")));
+				tmpMap.put("PROVPREST", String.valueOf(current.get("PROVPREST")));
+				tmpMap.put("CODDISTPREST", String.valueOf(current.get("CODDISTPREST")));
+				tmpMap.put("DISTPREST", String.valueOf(current.get("DISTPREST")));
+				tmpMap.put("DIRECPREST", String.valueOf(current.get("DIRECPREST")));
+				tmpMap.put("TIPOOPER", String.valueOf(current.get("TIPOOPER")));
+				tmpMap.put("NUMOPER", String.valueOf(current.get("NUMOPER")));
+				tmpMap.put("FECINIVIG", String.valueOf(current.get("FECINIVIG")));
+				tmpMap.put("FECFINVIG", String.valueOf(current.get("FECFINVIG")));
+				tmpMap.put("SUMAASEG", String.valueOf(current.get("SUMAASEG")));
+				tmpMap.put("CODTIPOPLAN", String.valueOf(current.get("CODTIPOPLAN")));
+				tmpMap.put("DESCRIPTIPOPLAN", String.valueOf(current.get("DESCRIPTIPOPLAN")));
+				tmpMap.put("CODPAISRIESGO", String.valueOf(current.get("CODPAISRIESGO")));
+				tmpMap.put("PAISRIESGO", String.valueOf(current.get("PAISRIESGO")));
+				tmpMap.put("CODDPTORIESGO", String.valueOf(current.get("CODDPTORIESGO")));
+				
+				tmpMap.put("DPTORIESGO", String.valueOf(current.get("DPTORIESGO")));
+				tmpMap.put("CODPROVRIESGO", String.valueOf(current.get("CODPROVRIESGO")));
+				tmpMap.put("PROVRIESGO", String.valueOf(current.get("PROVRIESGO")));
+				tmpMap.put("CODDISTRIESGO", String.valueOf(current.get("CODDISTRIESGO")));
+				tmpMap.put("DISTRIESGO", String.valueOf(current.get("DISTRIESGO")));
+				tmpMap.put("DIRECRIESGO", String.valueOf(current.get("DIRECRIESGO")));
+				tmpMap.put("CODCATCONSTRRIESGO", String.valueOf(current.get("CODCATCONSTRRIESGO")));
+				tmpMap.put("CATCONSRIESGO", String.valueOf(current.get("CATCONSRIESGO")));
+				tmpMap.put("TIPOEDIFRIESGO", String.valueOf(current.get("TIPOEDIFRIESGO")));
+				tmpMap.put("NUMSOTCONSTRIESGO", String.valueOf(current.get("NUMSOTCONSTRIESGO")));
+				tmpMap.put("NUMPISOCONSTRIESGO", String.valueOf(current.get("NUMPISOCONSTRIESGO")));
+				tmpMap.put("AÑOCONSTRIESGO", String.valueOf(current.get("ANIOCONSTRIESGO")));
+				tmpMap.put("USOSBS", String.valueOf(current.get("USOSBS")));
+				tmpMap.put("MONEDA", String.valueOf(current.get("MONEDA")));
+				tmpMap.put("PRIMA ANUAL", String.valueOf(current.get("PRIMAANUAL")));
+				tmpMap.put("TASA NETA ANUAL", String.valueOf(current.get("TASANETAANUAL")));
+				resultFinal.add(tmpMap);
+			}
+			
+		}
 		
-		return  result;
+		return  resultFinal;
 		
 	}
 	
